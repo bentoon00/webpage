5f4c2ae16be5e1276d9a4529941d1c094a0067d3
Ben <bentoon00@gmail.com>
2025-11-18 22:10:13 +0000

    Windows 7 redesign and adapter change

---
+++ .svelte-kit/output/server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2.Di2wYp3q.js","_app/immutable/chunks/LMxKtVA3.js","_app/immutable/chunks/GuuOCaML.js","_app/immutable/chunks/BupC4dte.js","_app/immutable/chunks/BXZb3Wr6.js"];
+export const stylesheets = [];
+export const fonts = [];
