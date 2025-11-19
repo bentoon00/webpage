ab886369fd1cb0a7d3219341d99e8950b5fa0741
Ben <bentoon00@gmail.com>
2025-11-19 16:11:59 +0000

    File restructuring, new temp favicon, title change

--- .svelte-kit/output/client/_app/immutable/entry/start.DIdWqLer.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Bp4a5qOn.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["robots.txt"]),
 	mimeTypes: {".txt":"text/plain"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DIdWqLer.js",app:"_app/immutable/entry/app.DnNG4iwp.js",imports:["_app/immutable/entry/start.DIdWqLer.js","_app/immutable/chunks/Bp4a5qOn.js","_app/immutable/chunks/GuuOCaML.js","_app/immutable/chunks/Cu_gUnPZ.js","_app/immutable/entry/app.DnNG4iwp.js","_app/immutable/chunks/GuuOCaML.js","_app/immutable/chunks/DI_Wr3xX.js","_app/immutable/chunks/BXZb3Wr6.js","_app/immutable/chunks/LMxKtVA3.js","_app/immutable/chunks/Cu_gUnPZ.js","_app/immutable/chunks/aHPi7g0r.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.Ckt34WBX.js",app:"_app/immutable/entry/app.Cu35F76j.js",imports:["_app/immutable/entry/start.Ckt34WBX.js","_app/immutable/chunks/B3_ATCj3.js","_app/immutable/chunks/GuuOCaML.js","_app/immutable/chunks/Cu_gUnPZ.js","_app/immutable/entry/app.Cu35F76j.js","_app/immutable/chunks/GuuOCaML.js","_app/immutable/chunks/DI_Wr3xX.js","_app/immutable/chunks/BXZb3Wr6.js","_app/immutable/chunks/LMxKtVA3.js","_app/immutable/chunks/Cu_gUnPZ.js","_app/immutable/chunks/aHPi7g0r.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
